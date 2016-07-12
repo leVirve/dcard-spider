5667ce10ca76914b4e7ea089cba11e6a3bbc4d5f
Salas <gae.m.project@gmail.com>
2016-07-12 21:02:12 +0800

    finish all basic functions migrated from script

--- demo.py
+++ demo.py
@@ -1,9 +1,18 @@
 from dcard import Dcard
+from pymongo import MongoClient
 from pprint import pprint as print
+
+
+client = MongoClient()
+db = client.dcard
+
 
 if __name__ == '__main__':
     dcard = Dcard()
 
-    #a = dcard.get_forums(no_school=True)
+    a = dcard.get_forums(no_school=True)
     b = dcard.get_post_ids('graduate', pages=2)
     print(len(b))
+
+    rid = db.forums.count()
+    print(rid)
