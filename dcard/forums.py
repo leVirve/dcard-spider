f27b8c4f62a140d32c18c906f0805c21265554a0
Salas <gae.m.project@gmail.com>
2016-07-14 16:15:57 +0800

    change middleware `metas` list to generator

--- dcard/forums.py
+++ dcard/forums.py
@@ -15,9 +15,13 @@ class Forum:
     def __init__(self, forum):
         self.forum = forum
 
-    def get_metas(self, pages=1, sort='new'):
+    def get_metas(self, pages=1, sort='new', callback=None):
         params = {'popular': True if sort == 'popular' else False}
-        return Forum.get_post_metas(self.forum, pages=pages, params=params)
+        results = []
+        for metas in Forum.get_post_metas(self.forum, pages=pages, params=params):
+            result = callback(metas) if callback else metas
+            results += result if isinstance(result, list) else results.append(result)
+        return results
 
     @staticmethod
     def get(**kwargs):
@@ -45,13 +49,11 @@ class Forum:
     @staticmethod
     def get_post_metas(forum, pages, params):
         logger.info('開始取得看板 [%s] 內文章資訊' % forum)
-        metas = []
         for _ in range(pages):
             data = Client.get(Forum.build_url(forum), params=params)
             try:
                 params['before'] = data[-1]['id']
-                metas += data
+                yield data
             except IndexError:
                 logger.warning('已到最末頁，第%d頁!' % _)
                 break
-        return metas
