d4e10c33bcb466ae9e373a53ad19781548d52c74
Salas <gae.m.project@gmail.com>
2016-07-25 12:05:03 +0800

    hotfix in cli for py2.6

--- dcard/cli.py
+++ dcard/cli.py
@@ -49,6 +49,6 @@ def download(args):
 
     print('成功下載 %d items！' % len(resources) if all(status)  \
             else '出了點錯下載不完全喔')
-    print('Finish in {:.5f} sec(s).'.format(time.time() - start_time))
+    print('Finish in {0:.5f} sec(s).'.format(time.time() - start_time))
 
     return all(status)
