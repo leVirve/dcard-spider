a3bf1e73d908cc672e7dfba4db68f1931526e0a1
Salas <gae.m.project@gmail.com>
2016-07-26 10:14:49 +0800

    make threadpool async tasks into separatable - Also make entire posts downloading module reducable with specific size

--- dcard/posts.py
+++ dcard/posts.py
@@ -8,6 +8,8 @@ from dcard.utils import client
 
 class Post:
 
+    reduce_threshold = 1000
+
     def __init__(self, metas):
         if isinstance(metas, list):
             first = metas[0]
@@ -22,22 +24,30 @@ class Post:
         bundle = {}
         if links:
             bundle['links_futures'] = [
-                client.sget(api.post_links_url_pattern.format(post_id=post_id))
-                for post_id in self.ids
+                [
+                    client.sget(api.post_links_url_pattern.format(post_id=post_id))
+                    for post_id in ids
+                ]
+                for ids in client.chunks(self.ids, chunck_size=Post.reduce_threshold)
             ]
         if content:
             bundle['content_futures'] = [
-                client.sget(api.post_url_pattern.format(post_id=post_id))
-                for post_id in self.ids
+                [
+                    client.sget(api.post_url_pattern.format(post_id=post_id))
+                    for post_id in ids
+                ]
+                for ids in client.chunks(self.ids, chunck_size=Post.reduce_threshold)
             ]
         if comments:
-            bundle['comments_async'] = \
-                client.parallel_tasks(Post._get_comments, self.ids)
+            bundle['comments_async'] = [
+                client.parallel_tasks(Post._serially_get_comments, ids)
+                for ids in client.chunks(self.ids, chunck_size=Post.reduce_threshold)
+            ]
 
         return PostsResult(self.ids, bundle, callback)
 
     @staticmethod
-    def _get_comments(post_id):
+    def _serially_get_comments(post_id):
         comments_url = api.post_comments_url_pattern.format(post_id=post_id)
 
         params = {}
@@ -54,8 +64,6 @@ class Post:
 
 class PostsResult:
 
-    reduce_threshold = 1000
-
     def __init__(self, ids, bundle, callback=None):
         self.ids = ids
         self.results = self.format(bundle, callback)
@@ -71,25 +79,23 @@ class PostsResult:
         return self.results[int(key)]
 
     def format(self, bundle, callback):
-        links = bundle.get('links_futures', [])
-        content = bundle.get('content_futures', [])
-        comments = bundle.get('comments_async')
-        comments = comments.get() if comments else []
+        links_blocks = bundle.get('links_futures', [])
+        content_blocks = bundle.get('content_futures', [])
+        comments_blocks = bundle.get('comments_async', [])
 
-        posts, results = [], []
-        for lnks, cont, cmts in zip_longest(links, content, comments):
-            post = {}
-            post.update(cont.result().json()) if cont else None
-            post.update({
-                'links': lnks.result().json() if lnks else None,
-                'comments': cmts,
-            })
-            posts.append(post)
-
-            if len(posts) >= PostsResult.reduce_threshold:
-                results.append(callback(posts) if callback else posts)
-                posts = []
-        results.append(callback(posts) if callback else posts)
+        results = []
+        for links, content, comments in zip(links_blocks, content_blocks, comments_blocks):
+            posts = []
+            comments = comments.get() if comments else []
+            for lnks, cont, cmts in zip_longest(links, content, comments):
+                post = {}
+                post.update(cont.result().json()) if cont else None
+                post.update({
+                    'links': lnks.result().json() if lnks else None,
+                    'comments': cmts,
+                })
+                posts.append(post)
+            results.append(callback(posts) if callback else posts)
 
         if len(results) and isinstance(results[0], list):
             results = client.flatten_result_lists(results)
