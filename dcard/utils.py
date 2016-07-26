c25d2b34eae469e72642d0f05bffa5b249711816
Salas <gae.m.project@gmail.com>
2016-07-26 20:09:25 +0800

    [feature] add exception handle in responses

--- dcard/utils.py
+++ dcard/utils.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import os
+import logging
 import itertools
 from multiprocessing.dummy import Pool
 
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from requests.exceptions import RetryError
 from requests_futures.sessions import FuturesSession
+
+logger = logging.getLogger('dcard')
 
 
 class Client:
 
-    def __init__(self):
-        self.fut_session = FuturesSession(max_workers=8)
+    def __init__(self, workers=8):
+        self.fut_session = FuturesSession(max_workers=workers)
         self.req_session = requests.Session()
-        self.thread_pool = Pool(processes=8)
+        self.thread_pool = Pool(processes=workers)
         self.retries = Retry(
             total=5,
             backoff_factor=0.1,
@@ -23,8 +27,15 @@ class Client:
         self.req_session.mount('https://', HTTPAdapter(max_retries=self.retries))
 
     def get(self, url, **kwargs):
-        response = self.req_session.get(url, **kwargs)
-        return response.json()
+        try:
+            response = self.req_session.get(url, **kwargs)
+            data = response.json()
+            if isinstance(data, dict) and data.get('error'):
+                logger.error('when get {}, error {}'.format(url, data))
+                return {}
+            return data
+        except RetryError as e:
+            logger.error('when get {}, error {}'.format(url, e))
 
     def get_stream(self, url, **kwargs):
         return self.req_session.get(url, stream=True, **kwargs)
