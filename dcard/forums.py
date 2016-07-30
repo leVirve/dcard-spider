87895cc914ac4ac7e1a52b2803c4fccb65215513
Salas <gae.m.project@gmail.com>
2016-07-30 14:27:25 +0800

    Add infinite_page control, and move page control into lower callee

--- dcard/forums.py
+++ dcard/forums.py
@@ -12,6 +12,8 @@ logger = logging.getLogger('dcard')
 class Forum:
 
     metas_per_page = 30
+    infinite_page = -1
+
     client = Client()
 
     def __init__(self, forum):
@@ -25,25 +27,26 @@ class Forum:
             return [forum for forum in Forum._extract_general(forums)]
         return forums
 
-    def get_metas(self,
-            num=30,
-            sort='new', timebound=None,
+    def get_metas(
+            self, num=30, sort='new', timebound=None,
             callback=None):
         logger.info('<%s> 開始取得看板內文章資訊' % self.forum)
 
-        pages = -(-num // self.metas_per_page)
-        paged_metas = self._get_paged_metas(pages, sort, timebound)
+        paged_metas = self._get_paged_metas(sort, num, timebound)
 
-        buff = flatten_lists((metas for metas in paged_metas))[:num]
+        buff = flatten_lists((metas for metas in paged_metas))
         results = callback(buff) if callback else buff
 
         logger.info('<%s> 資訊蒐集完成，共%d筆' % (self.forum, len(buff)))
         return results
 
-    def _get_paged_metas(self, pages, sort, timebound=''):
+    def _get_paged_metas(self, sort, num, timebound=''):
         params = {'popular': False} if sort == 'new' else {}
+        pages = -(-num // self.metas_per_page) if num >= 0 else self.infinite_page
 
-        for page in range(pages):
+        page = 0
+        while page != pages:
+            page += 1
             metas = self.client.get(self.posts_meta_url, params=params)
 
             if len(metas) == 0:
@@ -51,6 +54,9 @@ class Forum:
                 return
 
             params['before'] = metas[-1]['id']
+
+            if num and page == pages:
+                metas = metas[:num - (pages - 1) * self.metas_per_page]
 
             if timebound:
                 metas = [m for m in metas if m['updatedAt'] > timebound]
