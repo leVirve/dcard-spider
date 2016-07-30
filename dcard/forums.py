d663e297d8bc742f5ed83fd1696057b15e33b871
Salas <gae.m.project@gmail.com>
2016-07-30 13:54:59 +0800

    Add `timebound` in getting paged_metas :sparkles: - Time string format must be ISO 8601 which can use lexical sort as datetime   sorting.

--- dcard/forums.py
+++ dcard/forums.py
@@ -25,39 +25,37 @@ class Forum:
             return [forum for forum in Forum._extract_general(forums)]
         return forums
 
-    def get_metas(self, num=30, sort='new', callback=None):
-        logger.info('[%s] 開始取得看板內文章資訊' % self.forum)
+    def get_metas(self,
+            num=30,
+            sort='new', timebound=None,
+            callback=None):
+        logger.info('<%s> 開始取得看板內文章資訊' % self.forum)
 
         pages = -(-num // self.metas_per_page)
-        paged_metas = self._get_paged_metas(pages, sort)
-
-        # results = []
-        # for page, metas in enumerate(paged_metas, start=1):
-        #     if page == pages:
-        #         metas = metas[:num - (pages - 1) * self.metas_per_page]
-        #     results.append(callback(metas) if callback else metas) # buffer?
-        #
-        # if len(results) and isinstance(results[0], list):
-        #     results = flatten_lists(results)
+        paged_metas = self._get_paged_metas(pages, sort, timebound)
 
         buff = flatten_lists((metas for metas in paged_metas))[:num]
         results = callback(buff) if callback else buff
 
-        logger.info('[%s] 資訊蒐集完成，共%d筆' % (self.forum, len(buff)))
+        logger.info('<%s> 資訊蒐集完成，共%d筆' % (self.forum, len(buff)))
         return results
 
-    def _get_paged_metas(self, pages, sort):
+    def _get_paged_metas(self, pages, sort, timebound=''):
         params = {'popular': False} if sort == 'new' else {}
 
         for page in range(pages):
-            data = self.client.get(self.posts_meta_url, params=params)
+            metas = self.client.get(self.posts_meta_url, params=params)
 
-            if len(data) == 0:
+            if len(metas) == 0:
                 logger.warning('[%s] 已到最末頁，第%d頁!' % (self.forum, page))
                 return
 
-            params['before'] = data[-1]['id']
-            yield data
+            params['before'] = metas[-1]['id']
+
+            if timebound:
+                metas = [m for m in metas if m['updatedAt'] > timebound]
+
+            yield metas
 
     @staticmethod
     def _extract_general(forums):
