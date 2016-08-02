3f1c2bfbfda26a50e4c0fcbe796124d4bfdc7a3e
Salas <gae.m.project@gmail.com>
2016-08-02 19:59:01 +0800

    simplify api formats

--- dcard/api.py
+++ dcard/api.py
@@ -1,28 +1,7 @@
-API_ROOT = 'https://www.dcard.tw/_api'
-FORUMS = 'forums'
-POSTS = 'posts'
+DOMAIN = 'https://www.dcard.tw/'
 
-
-forums_url = '{api_root}/{api_forums}'.format(
-    api_root=API_ROOT,
-    api_forums=FORUMS
-)
-
-posts_meta_url_pattern = '{api_root}/{api_forums}/{{forum}}/{api_posts}'.format(
-    api_root=API_ROOT,
-    api_forums=FORUMS,
-    api_posts=POSTS,
-)
-
-post_url_pattern = '{api_root}/{api_posts}/{{post_id}}'.format(
-    api_root=API_ROOT,
-    api_posts=POSTS,
-)
-
-post_links_url_pattern = '{post_url}/links'.format(
-    post_url=post_url_pattern
-)
-
-post_comments_url_pattern = '{post_url}/comments'.format(
-    post_url=post_url_pattern
-)
+forums_url = DOMAIN + '_api/forums'
+post_url_pattern = DOMAIN + '_api/posts/{post_id}'
+posts_meta_url_pattern = DOMAIN + '_api/forums/{forum}/posts'
+post_links_url_pattern = DOMAIN + '_api/posts/{post_id}/links'
+post_comments_url_pattern = DOMAIN + '_api/posts/{post_id}/comments'
