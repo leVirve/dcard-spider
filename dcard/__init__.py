c8750e43a8745cf055fddb1432c6c17738c76952
Salas <gae.m.project@gmail.com>
2016-08-03 10:24:51 +0800

    remove all logging handlers in initialization

--- dcard/__init__.py
+++ dcard/__init__.py
@@ -7,20 +7,30 @@ from dcard.dcard import Dcard  # noqa
 __version__ = '0.2.9'
 
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+try:
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
 
-fh = logging.FileHandler('dcard.log', encoding='utf8')
-ch = logging.StreamHandler()
-fh.setLevel(logging.DEBUG)
-ch.setLevel(logging.WARNING)
+logging.getLogger(__name__).addHandler(NullHandler())
 
-fh_formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch_formatter = logging.Formatter(
-    '%(asctime)s - %(message)s')
-fh.setFormatter(fh_formatter)
-ch.setFormatter(ch_formatter)
-logger.addHandler(fh)
-logger.addHandler(ch)
-# remova handlers
+
+def add_handles_on_logger():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('dcard.log', encoding='utf8')
+    ch = logging.StreamHandler()
+    fh.setLevel(logging.DEBUG)
+    ch.setLevel(logging.WARNING)
+
+    fh_formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch_formatter = logging.Formatter(
+        '%(asctime)s - %(message)s')
+    fh.setFormatter(fh_formatter)
+    ch.setFormatter(ch_formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
